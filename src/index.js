65f69f4c133bbc3564e16dfe52f178ab1b413b67
MatthiasVanHecke <71766401+MatthiasVanHecke@users.noreply.github.com>
2023-10-12 11:53:34 +0200

    feat: Read by id

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const app = express();
 const db = knex(knexConfig.development);
 app.use(bodyParser.json());
 
+// Read One
+app.get('/artworks/:id', async (req, res) => {
+    const id = req.params.id;  
+    try {
+        const artwork = await db('artworks').where({ id }).first();
+        if (!artwork) {
+          return res.status(404).json({ error: 'Artwork not found' });
+        }
+        res.json(artwork);
+      } catch (err) {
+        res.status(500).json({ error: 'Error retrieving artwork' });
+      }
+    });
+
 // Retrieve all artworks
 app.get('/artworks', (req, res) => {
     db('artworks')
